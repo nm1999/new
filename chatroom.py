9cb5eb8a730212b377ff083f02946581709faabf
nm1999 <matianyanzi@gmail.com>
2021-09-27 01:11:25 +0200

    second commit

---
+++ chatroom.py
@@ -0,0 +1,17 @@
+print("Hello")
+name = input("What is your name? ")
+print("Hi " + name)
+prob = print("How can i help u " + name + " ? ")
+answer = input("")
+print("Really")
+reply = input("")
+location = input("Where do u live? ")
+loca_qn = input(" " + name + " are  sure you live in " + location + " ? ")
+dob = input("In which year were you born"+ name + "? ")
+age = 2020 - int(dob)
+print("So " + name + " Your ")
+print(age)
+print("old")
+
+
+
